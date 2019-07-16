4dbb6de33b4f842f6d761931b9cab56a09a74f5f
abdev <abdusalam.yab1996@gmail.com>
2019-07-16 22:17:46 +0200

    Changed the font of all the text to Segoe UI

--- app/src/main/java/com/example/highschoolgrades/PopulateDbAsync.java
+++ app/src/main/java/com/example/highschoolgrades/PopulateDbAsync.java
@@ -1,10 +1,6 @@
 package com.example.highschoolgrades;
 
 import android.os.AsyncTask;
-import android.util.Log;
-
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 public class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
@@ -16,7 +12,6 @@ public class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected Void doInBackground(Void... voids) {
-        Log.e("Count", "doInBackground: " + mDao.getCount().size());
         if (mDao.getCount().size() <= 0) {
             for (Course c : Constants.highSchoolSharedCourses) mDao.insert(c);
         }
